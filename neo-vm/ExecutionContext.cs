29f53a91f33ebdbaa782455d47e2773975791de3
Dmitry Ivashchenko <dmitry.ivashchenko@latoken.com>
2018-10-30 17:03:46 +0300

    [+] initial bulk-commit

---
+++ neo-vm/ExecutionContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Neo.VM
+{
+    public class ExecutionContext : IDisposable
+    {
+        public readonly byte[] Script;
+        internal readonly int RVCount;
+        internal readonly BinaryReader OpReader;
+        private readonly ICrypto crypto;
+
+        public RandomAccessStack<StackItem> EvaluationStack { get; } = new RandomAccessStack<StackItem>();
+        public RandomAccessStack<StackItem> AltStack { get; } = new RandomAccessStack<StackItem>();
+
+        public int InstructionPointer
+        {
+            get
+            {
+                return (int)OpReader.BaseStream.Position;
+            }
+            set
+            {
+                OpReader.BaseStream.Seek(value, SeekOrigin.Begin);
+            }
+        }
+
+        public OpCode NextInstruction => (OpCode)Script[OpReader.BaseStream.Position];
+
+        private byte[] _script_hash = null;
+        public byte[] ScriptHash
+        {
+            get
+            {
+                if (_script_hash == null)
+                    _script_hash = crypto.Hash160(Script);
+                return _script_hash;
+            }
+        }
+
+        internal ExecutionContext(ExecutionEngine engine, byte[] script, int rvcount)
+        {
+            this.Script = script;
+            this.RVCount = rvcount;
+            this.OpReader = new BinaryReader(new MemoryStream(script, false));
+            this.crypto = engine.Crypto;
+        }
+
+        public void Dispose()
+        {
+            OpReader.Dispose();
+        }
+    }
+}
---
+++ neo/IO/ISerializable.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Neo.IO
+{
+    public interface ISerializable
+    {
+        int Size { get; }
+
+        void Serialize(BinaryWriter writer);
+        void Deserialize(BinaryReader reader);
+    }
+}
