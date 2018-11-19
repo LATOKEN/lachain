5887811983ae91243496e8ee9c084045d3529733
Dmitry Savonin <dmitry.savonin@latoken.com>
2018-11-19 19:00:20 +0300

    [*] refactored consensus config. [*] changed main config file.

---
+++ Phorkus/Phorkus.Core/Blockchain/Consensus/ConsensusConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Phorkus.Core.Proto;
+
+namespace Phorkus.Core.Blockchain.Consensus
+{
+    public class ConsensusConfig
+    {
+        [JsonProperty("validators")]
+        public List<PublicKey> ValidatorsKeys { get; set; }
+
+        [JsonProperty("privateKey")]
+        public string PrivateKey { get; set; }
+    }
+}
