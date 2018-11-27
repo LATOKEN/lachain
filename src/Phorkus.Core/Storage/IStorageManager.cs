683d0893f5fc5ffc008adea8a3d1064eece93d02
Dmitry Savonin <dmitry.savonin@latoken.com>
2018-11-27 17:06:23 +0300

    [*] added current version property to storage manager.

--- src/Phorkus.Core/Storage/IStorageManager.cs
+++ src/Phorkus.Core/Storage/IStorageManager.cs
@@ -4,6 +4,8 @@ namespace Phorkus.Core.Storage
 {    
     public interface IStorageManager
     {
+        long Version { get; }
+
         TValue Get<TKey, TValue>(ulong version, TKey key)
             where TKey : IMessage
             where TValue : IMessage;
