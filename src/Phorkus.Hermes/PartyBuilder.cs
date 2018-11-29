b63811d039013c7591abe94a6321526eb10cd3b1
Dmitry Savonin <dmitry.savonin@latoken.com>
2018-11-29 11:48:09 +0300

    [*] fixed tiny errors.

--- src/Phorkus.Hermes/PartyBuilder.cs
+++ src/Phorkus.Hermes/PartyBuilder.cs
@@ -1,6 +1,40 @@
-﻿namespace Phorkus.Hermes
+﻿using System.Collections.Generic;
+using Phorkus.Proto;
+
+namespace Phorkus.Hermes
 {
-    /*public class PartyBuilder : IPartyBuilder
-    { 
-    }*/
+    public class PartyBuilder : IPartyBuilder
+    {
+        public PartyState CurrentState { get; }
+        
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BgwPublicParams GenerateShare()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CollectShare(IReadOnlyCollection<BgwPublicParams> shares)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BgwnPoint GeneratePoint()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CollectPoint(IReadOnlyCollection<BgwnPoint> points)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Finalize()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
