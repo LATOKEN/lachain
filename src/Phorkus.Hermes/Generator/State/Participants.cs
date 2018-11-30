8d7b8d41527926a29882d3359d6a607601feb4d4
Dmitry Savonin <dmitry.savonin@latoken.com>
2018-11-30 17:45:23 +0300

    [*] moved participants to another folder.

---
+++ src/Phorkus.Hermes/Generator/State/Participants.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Phorkus.Proto;
+
+namespace Phorkus.Hermes.Generator.State
+{
+    public class Participants
+    {
+        private IReadOnlyDictionary<PublicKey, int> participants;
+
+        public Participants(IDictionary<PublicKey, int> participants)
+        {
+            this.participants = new Dictionary<PublicKey, int>(participants);
+        }
+
+        /** @return the map containing the mapping between ActorRef's and id in the protocol*/
+        public IReadOnlyDictionary<PublicKey, int> GetParticipants()
+        {
+            return participants;
+        }
+    }
+}
