9ef518496cf4ea3ddc3d2d13961d6b6edb6cc9e8
Dmitry Savonin <dmitry.savonin@latoken.com>
2019-01-23 16:01:33 +0300

    [*] added external handler for default WASM environment.

---
+++ src/Phorkus.Core/VM/EnvExternalHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Phorkus.WebAssembly;
+
+namespace Phorkus.Core.VM
+{
+    public class EnvExternalHandler : IExternalHandler
+    {
+        private const string EnvModule = "env";
+
+        public static int Handler_Env_GetCallValue(int offset)
+        {
+            return 0;
+        }
+        
+        public IEnumerable<FunctionImport> GetFunctionImports()
+        {
+            return new[]
+            {
+                new FunctionImport(EnvModule, "getcallvalue", typeof(EthereumExternalHandler).GetMethod(nameof(Handler_Env_GetCallValue)))
+            };
+        }
+    }
+}
