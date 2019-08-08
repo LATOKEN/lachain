985ef2592c9158aa68dce3f4d738d26cc3a52b16
Artsem Zhuk <artem.zhuk@latoken.com>
2019-08-08 15:02:44 +0300

    added SilentProtocol abstraction

---
+++ test/Phorkus.ConsensusTest/SilentProtocol.cs
@@ -0,0 +1,32 @@
+﻿using Phorkus.Consensus;
+using Phorkus.Consensus.Messages;
+
+namespace Phorkus.ConsensusTest
+{
+    public class SilentProtocol<TId> : IConsensusProtocol where TId : IProtocolIdentifier
+    {
+        public SilentProtocol(TId id)
+        {
+            Id = id;
+        }
+        
+        public override void ProcessMessage(MessageEnvelope envelope)
+        {
+        }
+
+        public IProtocolIdentifier Id { get; }
+        public void ReceiveMessage(MessageEnvelope message)
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void WaitFinish()
+        {
+        }
+
+        public bool Terminated => true;
+    }
+}
