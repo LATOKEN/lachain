cde04d4efc9d2b5ab03e0993469595a7ce1692f7
Dmitry Ivashchenko <dmitry.ivashchenko@latoken.com>
2021-04-09 10:53:48 +0300

    @LACHAIN-0 update VM test with empty contract

--- test/Lachain.CoreTest/IntegrationTests/VirtualMachineTest.cs
+++ test/Lachain.CoreTest/IntegrationTests/VirtualMachineTest.cs
@@ -1,10 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
+using Lachain.Core.Blockchain.VM;
 using Lachain.Core.CLI;
 using Lachain.Core.Config;
 using Lachain.Core.DI;
 using Lachain.Core.DI.Modules;
 using Lachain.Core.DI.SimpleInjector;
+using Lachain.Proto;
+using Lachain.Storage.State;
+using Lachain.Utility;
+using Lachain.Utility.Utils;
 using Lachain.UtilityTest;
 using NUnit.Framework;
 
@@ -44,23 +50,19 @@ namespace Lachain.CoreTest.IntegrationTests
         [Test]
         public void Test_VirtualMachine_InvokeContract()
         {
-            // TODO: rewrite this test
-            // var virtualMachine = _container.Resolve<IVirtualMachine>();
-            //
-            // var sender = "0x6bc32575acb8754886dc283c2c8ac54b1bd93195".HexToBytes().ToUInt160();
-            // var input = "0xffffffffffffffff".HexToBytes();
-            //
-            // var byteCode =
-            //     "0061736d010000000117056000017f60037f7f7f0060027f7f0060000060017f0002360303656e760d636f707963616c6c76616c7565000103656e760b67657463616c6c73697a65000003656e760877726974656c6f6700020304030304040404017000000503010001071202066d656d6f7279020005737461727400030a59034301017f4100410028020441106b2200360204410010014110100020004200370308200041086a1004410020002903083703104110410810024100200041106a3602040b0600200010050b0c00200041002903103703000b"
-            //         .HexToBytes();
-            // var contract = new Contract
-            // {
-            //     ContractAddress = UInt160Utils.Zero,
-            //     ByteCode = ByteString.CopyFrom(byteCode)
-            // };
+            var stateManager = _container.Resolve<IStateManager>();
+            var tx = new TransactionReceipt();
+            
+            var sender = "0x6bc32575acb8754886dc283c2c8ac54b1bd93195".HexToBytes().ToUInt160();
+            var input = "0xffffffffffffffff".HexToBytes();
+            
+            var byteCode =
+                "0061736d01000000018480808000016000000382808080000100048480808000017000000583808080000100010681808080000007928080800002066d656d6f7279020005737461727400000a8880808000018280808000000b"
+                    .HexToBytes();
+            var contract = new Contract(UInt160Utils.Zero, byteCode);
 
-            // var status = virtualMachine.InvokeWasmContract(contract, new InvocationContext(sender), input, 100_000_000);
-            // Console.WriteLine("Contract executed with status: " + status);
+            var status = VirtualMachine.InvokeWasmContract(contract, new InvocationContext(sender, stateManager.LastApprovedSnapshot, tx), input, 100_000_000);
+            Console.WriteLine("Contract executed with status: " + status);
         }
     }
 }
