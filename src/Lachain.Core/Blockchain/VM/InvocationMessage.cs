66364bf92c7833a512cbd7d6d62bd47181b5e843
pavel.soloviev <pavel.soloviev@latoken.com>
2021-07-09 17:39:00 +0300

    Added InvocationMessage

---
+++ src/Lachain.Core/Blockchain/VM/InvocationMessage.cs
@@ -0,0 +1,11 @@
+﻿using Lachain.Proto;
+
+namespace Lachain.Core.Blockchain.VM
+{
+    class InvocationMessage
+    {
+        public UInt160 Sender { get; set; }
+        public UInt256 Value { get; set; }
+        public byte[] Sig { get; set; }
+    }
+}
