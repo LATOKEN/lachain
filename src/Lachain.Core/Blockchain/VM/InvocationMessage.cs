15b040af3b8bfac2861c2e2b9e814f280c0491ac
pavel.soloviev <pavel.soloviev@latoken.com>
2021-07-19 16:34:50 +0300

    Added external handler for delegateCall

--- src/Lachain.Core/Blockchain/VM/InvocationMessage.cs
+++ src/Lachain.Core/Blockchain/VM/InvocationMessage.cs
@@ -6,5 +6,8 @@ namespace Lachain.Core.Blockchain.VM
     {
         public UInt160 Sender { get; set; }
         public UInt256 Value { get; set; }
+        public InvocationType Type { get; set; }
+        public UInt160? Delegate { get; set; }
+
     }
 }
---
+++ src/Lachain.Core/Blockchain/VM/InvocationType.cs
@@ -0,0 +1,9 @@
+﻿namespace Lachain.Core.Blockchain.VM
+{
+    public enum InvocationType
+    {
+        Regular = 0,
+        Static = 1,
+        Delegate = 2,
+    }
+}
