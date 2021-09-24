aeee1dfdc21cc5a57112ca137be5f9a0164107e2
sgladkov <sergey.gladkov@latoken.com>
2021-09-24 12:18:31 +0300

    Update version to 0.0.38

--- src/Lachain.Core/Network/NetworkManager.cs
+++ src/Lachain.Core/Network/NetworkManager.cs
@@ -7,8 +7,8 @@ namespace Lachain.Core.Network
 {
     public class NetworkManager : NetworkManagerBase
     {
-        public const int MyVersion = 10;
-        public const int MinCompatiblePeerVersion = 10;
+        public const int MyVersion = 11;
+        public const int MinCompatiblePeerVersion = 11;
         
         public NetworkManager(
             IConfigManager configManager, IPrivateWallet privateWallet
