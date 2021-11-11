2808b5e7b70629fe79b4ce082dcf58dfcf3e44b6
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2021-11-11 18:24:52 +0530

    removed definetion

--- src/Lachain.Core/Blockchain/Pool/ITransactionPool.cs
+++ src/Lachain.Core/Blockchain/Pool/ITransactionPool.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Lachain.Core.Blockchain.Error;
 using Lachain.Proto;
-using System.Threading.Tasks;
 
 namespace Lachain.Core.Blockchain.Pool
 {
@@ -19,8 +18,6 @@ namespace Lachain.Core.Blockchain.Pool
         OperatingError Add(Transaction transaction, Signature signature, bool notify = true);
 
         OperatingError Add(TransactionReceipt receipt, bool notify = true);
-
-        Task<OperatingError> AddPrlAsync(Transaction transaction, Signature signature, bool notify = true);
 
         IReadOnlyCollection<TransactionReceipt> Peek(int txsToLook, int txsToTake);
 
