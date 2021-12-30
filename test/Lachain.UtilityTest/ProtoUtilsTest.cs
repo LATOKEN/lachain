5b28d56d68e7005cb2e9add4169e560a97234402
HMAshiqulIslam <ashiqul.islam@latoken.com>
2021-12-30 15:07:52 +0600

    added unit tests for decoding and encoding of proposed transaction_receipts_list

---
+++ test/Lachain.UtilityTest/ProtoUtilsTest.cs
@@ -0,0 +1,74 @@
+using System.Numerics;
+using Lachain.Proto;
+using Lachain.Utility;
+using Lachain.Utility.Utils;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Lachain.UtilityTest
+{
+    public class ProtoUtilsTest
+    {
+        [Test]
+        [Repeat(10)]
+        public void Test_ToMessageArrayCorrect()
+        {
+            List<TransactionReceipt> receipts = new List<TransactionReceipt>();
+            for(int i = 0; i < 100; i++) {
+                receipts.Add(TestUtils.GetRandomTransaction());
+            }
+            var decoded = receipts.ToByteArray();
+            var encoded = decoded.ToMessageArray<TransactionReceipt>();
+            Assert.True(receipts.SequenceEqual(encoded));
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void Test_ToMessageArrayExtraBytesAtEnd()
+        {
+            List<TransactionReceipt> receipts = new List<TransactionReceipt>();
+            for(int i = 0; i < 100; i++) {
+                receipts.Add(TestUtils.GetRandomTransaction());
+            }
+            var decoded = receipts.ToByteArray();
+            var junk = new byte[] {1, 2, 3};
+            decoded = decoded.Concat(junk).ToArray();
+
+            bool caughtException = false; 
+            try
+            {
+                var encoded = decoded.ToMessageArray<TransactionReceipt>();
+                Assert.IsTrue(1 == 2);
+            }
+            catch(Exception e)
+            {
+                // adding junk at the end should throw Exception;
+                caughtException = true;
+            }
+            Assert.IsTrue(caughtException == true);
+        }
+
+        [Test]
+        public void Test_ToMessageArrayRandomBytes()
+        {
+            var decoded = new byte[100];
+            for(var i = 0; i < 100; i++) 
+                decoded[i] = (byte) (i % 256);
+            
+            bool caughtException = false; 
+            try
+            {
+                var encoded = decoded.ToMessageArray<TransactionReceipt>();
+                Assert.IsTrue(1 == 2);
+            }
+            catch(Exception e)
+            {
+                // adding junk at the end should throw Exception;
+                caughtException = true;
+            }
+            Assert.IsTrue(caughtException == true);
+        }
+    }
+}
