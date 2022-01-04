547fb9fda051526fb68e7c5fb1c96b7522749c3c
Arghya <arghyapal007@gmail.com>
2022-01-04 19:42:21 +0600

    batch size of blockHeaderDOwnloader changed(1000),  TryGetBatch issue with(Min) fixed

--- src/Lachain.Core/Network/FastSynchronizerBatch/BlockRequestManager.cs
+++ src/Lachain.Core/Network/FastSynchronizerBatch/BlockRequestManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
@@ -10,16 +9,19 @@ using Lachain.Storage.Trie;
 using Lachain.Utility.Utils;
 using Lachain.Core.RPC.HTTP.Web3;
 using Lachain.Storage.State;
+using Lachain.Logger;
+
 
 namespace Lachain.Core.Network.FastSynchronizerBatch
 {
     class BlockRequestManager
     {
+        private static readonly ILogger<BlockRequestManager> Logger = LoggerFactory.GetLoggerForClass<BlockRequestManager>();
  //       private Queue<string> _queue = new Queue<string>();
         private SortedSet<ulong> _pending = new SortedSet<ulong>();
         private SortedSet<ulong> nextBlocksToDownload = new SortedSet<ulong>();
         private IDictionary<ulong, string> downloaded = new Dictionary<ulong,string>();
-        private uint _batchSize = 40;
+        private uint _batchSize = 1000;
         ulong _done = 0;
         ulong _maxBlock;
 
@@ -30,66 +32,57 @@ namespace Lachain.Core.Network.FastSynchronizerBatch
         {
             _nodeStorage = nodeStorage;
             _blockSnapshot = blockSnapshot;
-            _done = 0;
+            _done = blockSnapshot.GetTotalBlockHeight();
             _maxBlock = maxBlock;
-            for(ulong i=1; i<=_maxBlock; i++) nextBlocksToDownload.Add(i);
+            for(ulong i=_done+1; i<=_maxBlock; i++) nextBlocksToDownload.Add(i);
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public bool TryGetBatch(out List<string> batch)
         {
             batch = new List<string>();
-            lock(this)
+            for(ulong i=0; i<_batchSize && nextBlocksToDownload.Count>0; i++)
             {
-                for(ulong i=0; i<_batchSize && nextBlocksToDownload.Count>0; i++)
-                {
-                    ulong blockId = nextBlocksToDownload.Min();
-                    _pending.Add(blockId);
-                    batch.Add(HexUtils.ToHex(blockId));
-                    nextBlocksToDownload.Remove(blockId);
-                } 
-            }
+                ulong blockId = nextBlocksToDownload.Min;
+                _pending.Add(blockId);
+                batch.Add(HexUtils.ToHex(blockId));
+                nextBlocksToDownload.Remove(blockId);
+            } 
             if (batch.Count == 0) return false;
 
             return true;
         }
         
-        [MethodImpl(MethodImplOptions.Synchronized)]
         public bool Done()
         {
             return _done==_maxBlock && _pending.Count==0;
         }
 
-
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public void HandleResponse(List<string> batch, JArray response)
         {
-            if(batch.Count>0) Console.WriteLine("First Node in this batch: "+Convert.ToUInt64(batch[0], 16));
+            if(batch.Count>0) Logger.LogInformation("First Node in this batch: "+Convert.ToUInt64(batch[0], 16));
 
             if(batch.Count != response.Count)
             {
-                lock(this)
+                foreach(var block in batch)
                 {
-                    foreach(var block in batch)
+                    ulong blockId = Convert.ToUInt64(block, 16);
+                    if(_pending.Contains(blockId))
                     {
-                        ulong blockId = Convert.ToUInt64(block, 16);
-                        if(_pending.Contains(blockId))
-                        {
-                            _pending.Remove(blockId);
-                            nextBlocksToDownload.Add(blockId);
-                        }
+                        _pending.Remove(blockId);
+                        nextBlocksToDownload.Add(blockId);
                     }
                 }
             }
             else{
-                lock(this)
+                for(int i=0; i<batch.Count; i++)
                 {
-                    for(int i=0; i<batch.Count; i++)
+                    ulong blockId = Convert.ToUInt64(batch[i], 16);
+                    if(_pending.Contains(blockId))
                     {
-                        ulong blockId = Convert.ToUInt64(batch[i], 16);
-                        if(_pending.Contains(blockId))
-                        {
-                            _pending.Remove(blockId);
-                             downloaded[blockId] = (string)response[i]; 
-                        }
+                        _pending.Remove(blockId);
+                        downloaded[blockId] = (string)response[i];
                     }
                 }
             }
@@ -99,17 +92,15 @@ namespace Lachain.Core.Network.FastSynchronizerBatch
         [MethodImpl(MethodImplOptions.Synchronized)]
         void AddToDB()
         {
+            ulong cnt = _done ;
+            DateTime start = DateTime.Now;
             while(downloaded.TryGetValue(_done+1, out var blockRawHex))
             {
                 _nodeStorage.AddBlock(_blockSnapshot, blockRawHex);
                 _done++;
+                
                 downloaded.Remove(_done);
             }
-    /*        if(downloaded.Count>0) Console.WriteLine("More blocks downloaded. Done: " + 
-            _done + " downloaded: "+downloaded.Count+" NextBlockToDownload " +
-            nextBlocksToDownload.Count+ " pending: "+_pending.Count+" Min pending: "+ _pending.Min()
-            +" Min to download: "+nextBlocksToDownload.Min());
-*/
         }
     }
 }
