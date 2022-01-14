ea0e6a986bb0de66e4e0b130ad689cdbe2342ca6
HMAshiqulIslam <ashiqul.islam@latoken.com>
2022-01-14 21:12:53 +0600

    added balance check and refactored pool

--- src/Lachain.Core/Blockchain/Pool/ITransactionPool.cs
+++ src/Lachain.Core/Blockchain/Pool/ITransactionPool.cs
@@ -21,13 +21,9 @@ namespace Lachain.Core.Blockchain.Pool
 
         OperatingError Add(TransactionReceipt receipt, bool notify = true);
 
-        IReadOnlyCollection<TransactionReceipt> Peek(int txsToLook, int txsToTake);
-
-        void Relay(IEnumerable<TransactionReceipt> receipts);
+        IReadOnlyCollection<TransactionReceipt> Peek(int txsToLook, int txsToTake, ulong era = 0);
 
         uint Size();
-
-        void Delete(UInt256 transactionHash);
         void Clear();
         void ClearRepository();
         ulong GetNextNonceForAddress(UInt160 address);
