3c4812d8bce77e12b17d70955a74579688d6329f
Tariq <tariq.salam@latoken.com>
2022-02-11 21:07:07 +0600

    Formatted response according to standard api response for events

--- src/Lachain.Utility/JSON/EventJsonConverter.cs
+++ src/Lachain.Utility/JSON/EventJsonConverter.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json.Linq;
 using Lachain.Proto;
 using Lachain.Utility.Utils;
 
@@ -9,13 +10,24 @@ namespace Lachain.Utility.JSON
         public static JObject ToJson(this EventObject evObj)
         {
             var ev = evObj._event;
+            if(ev is null) throw new Exception("event is null");
+            var topics = new JArray();
+            topics.Add(ev.SignatureHash.ToHex());
+            var topicList = evObj._topics;
+            if(topicList != null)
+            {
+                foreach(var topic in topicList)
+                {
+                    topics.Add(topic.ToHex());
+                }
+            }
             var json = new JObject
             {
                 ["contract"] = ev!.Contract.ToHex(),
                 ["data"] = ev.Data.ToHex(),
                 ["transactionHash"] = ev.TransactionHash.ToHex(),
                 ["index"] = ev.Index,
-                ["signatureHash"] = ev.SignatureHash.ToHex()
+                ["topics"] = topics
             };
             return json;
         }
