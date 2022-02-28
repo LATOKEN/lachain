aeec9ccde91e822a4d618bd4067ec472539b0b7b
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2022-02-28 18:02:58 +0900

    RootProtocolId

--- src/Lachain.Consensus/RootProtocol/RootProtocolId.cs
+++ src/Lachain.Consensus/RootProtocol/RootProtocolId.cs
@@ -1,3 +1,9 @@
+using Lachain.Utility.Serialization;
+using Nethereum.RLP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Lachain.Consensus.RootProtocol
 {
     public class RootProtocolId : IProtocolIdentifier
@@ -27,7 +33,7 @@ namespace Lachain.Consensus.RootProtocol
 
         public override bool Equals(object? obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             return obj.GetType() == GetType() && Equals((RootProtocolId) obj);
         }
@@ -35,6 +41,24 @@ namespace Lachain.Consensus.RootProtocol
         public override int GetHashCode()
         {
             return Era.GetHashCode();
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytesArray = new List<byte[]>
+            {
+                Era.ToBytes().ToArray(),
+            };
+
+            return RLP.EncodeList(bytesArray.Select(RLP.EncodeElement).ToArray());
+        }
+
+        public static RootProtocolId FromBytes(ReadOnlyMemory<byte> bytes)
+        {
+            var decoded = (RLPCollection)RLP.Decode(bytes.ToArray());
+            var era = decoded[0].RLPData.AsReadOnlySpan().ToInt64();
+
+            return new RootProtocolId(era);
         }
     }
 }
