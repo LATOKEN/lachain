b6fc6b68a0aa5882b58327f16d46c83cebd3462f
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2022-02-28 17:43:21 +0900

    ReliableBroadcastId

--- src/Lachain.Consensus/ReliableBroadcast/ReliableBroadcastId.cs
+++ src/Lachain.Consensus/ReliableBroadcast/ReliableBroadcastId.cs
@@ -1,4 +1,8 @@
+using Lachain.Utility.Serialization;
+using Nethereum.RLP;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lachain.Consensus.ReliableBroadcast
 {
@@ -16,9 +20,9 @@ namespace Lachain.Consensus.ReliableBroadcast
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((ReliableBroadcastId) obj);
         }
 
@@ -31,15 +35,35 @@ namespace Lachain.Consensus.ReliableBroadcast
 
         public long Era { get; }
 
-        public ReliableBroadcastId(int validator, int era)
+        public ReliableBroadcastId(int validator, long era)
         {
             SenderId = validator;
-            Era = (long) era;
+            Era = era;
         }
 
         public override string ToString()
         {
             return $"RBC (E={Era}, A={SenderId})";
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytesArray = new List<byte[]>
+            {
+                SenderId.ToBytes().ToArray(),
+                Era.ToBytes().ToArray(),
+            };
+
+            return RLP.EncodeList(bytesArray.Select(RLP.EncodeElement).ToArray());
+        }
+
+        public static ReliableBroadcastId FromBytes(ReadOnlyMemory<byte> bytes)
+        {
+            var decoded = (RLPCollection)RLP.Decode(bytes.ToArray());
+            var senderId = decoded[0].RLPData.AsReadOnlySpan().ToInt32();
+            var era = decoded[1].RLPData.AsReadOnlySpan().ToInt64();
+
+            return new ReliableBroadcastId(senderId, era);
         }
     }
 }
