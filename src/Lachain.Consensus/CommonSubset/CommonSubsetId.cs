9d9f07f811142667b525d14dbd35620c0a55c9c7
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2022-02-28 17:33:36 +0900

    CommonSubset

--- src/Lachain.Consensus/CommonSubset/CommonSubsetId.cs
+++ src/Lachain.Consensus/CommonSubset/CommonSubsetId.cs
@@ -1,4 +1,9 @@
 ﻿using Lachain.Consensus.HoneyBadger;
+using Lachain.Utility.Serialization;
+using Nethereum.RLP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lachain.Consensus.CommonSubset
 {
@@ -33,15 +38,33 @@ namespace Lachain.Consensus.CommonSubset
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((CommonSubsetId) obj);
         }
 
         public override int GetHashCode()
         {
             return Era.GetHashCode();
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytesArray = new List<byte[]>
+            {
+                Era.ToBytes().ToArray()
+            };
+
+            return RLP.EncodeList(bytesArray.Select(RLP.EncodeElement).ToArray());
+        }
+
+        public static CommonSubsetId FromBytes(ReadOnlyMemory<byte> bytes)
+        {
+            var decoded = (RLPCollection)RLP.Decode(bytes.ToArray());
+            var era = decoded[0].RLPData.AsReadOnlySpan().ToInt64();
+
+            return new CommonSubsetId(era);
         }
     }
 }
