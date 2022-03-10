814ee1999e9aef68534c28c9f1c8896055237a6e
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2022-03-11 01:33:36 +0900

    Update ErasureCoding.cs

--- src/Lachain.Consensus/ReliableBroadcast/ErasureCoding.cs
+++ src/Lachain.Consensus/ReliableBroadcast/ErasureCoding.cs
@@ -1,12 +1,18 @@
 using Lachain.Consensus.ReliableBroadcast.ReedSolomon.ReedSolomon;
 using Lachain.Logger;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace Lachain.Consensus.ReliableBroadcast
 {
+    [DataContract]
     public class ErasureCoding
     {
+        [DataMember]
         private static readonly ILogger<ErasureCoding> Logger = LoggerFactory.GetLoggerForClass<ErasureCoding>();
 
+        [DataMember]
         private readonly GenericGF _field;
         
         public ErasureCoding()
@@ -27,6 +33,29 @@ namespace Lachain.Consensus.ReliableBroadcast
             Logger.LogError($"Too many errors-erasures to correct. Additional bits = {additionalBits}");
             Logger.LogError("Code: " + string.Join(", ", encryptionText));
             Logger.LogError("Tips: " + string.Join(", ", tips));
+        }
+
+        public byte[] ToBytes()
+        {
+            using var ms = new MemoryStream();
+            var serializer = new DataContractSerializer(typeof(ErasureCoding));
+            serializer.WriteObject(ms, this);
+
+            return ms.ToArray();
+        }
+
+        public static ErasureCoding? FromBytes(ReadOnlyMemory<byte> bytes)
+        {
+            if(bytes.ToArray() == null)
+            {
+                return default;
+            }
+
+            using var memStream = new MemoryStream(bytes.ToArray());
+            var serializer = new DataContractSerializer(typeof(ErasureCoding));
+            var obj = (ErasureCoding?)serializer.ReadObject(memStream);
+
+            return obj;
         }
     }
 }
