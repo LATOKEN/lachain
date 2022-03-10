40f8092fbd255c9d91fe9a2a594a5140111da54e
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2022-03-11 00:29:34 +0900

    Update Commitment.cs

--- src/Lachain.Consensus/ThresholdKeygen/Data/Commitment.cs
+++ src/Lachain.Consensus/ThresholdKeygen/Data/Commitment.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using Lachain.Utility.Utils;
 using MCL.BLS12_381.Net;
 
 namespace Lachain.Consensus.ThresholdKeygen.Data
 {
+    [DataContract]
     public class Commitment : IEquatable<Commitment>
     {
+        [DataMember]
         private readonly G1[] _coefficients;
+        [DataMember]
         public readonly int Degree;
 
         internal Commitment(IEnumerable<G1> g1)
@@ -53,16 +58,16 @@ namespace Lachain.Consensus.ThresholdKeygen.Data
             return row;
         }
 
-        private int Index(int i, int j)
+        private static int Index(int i, int j)
         {
             if (i > j) (i, j) = (j, i);
             return i * (i + 1) / 2 + j;
         }
 
-        public byte[] ToBytes()
-        {
-            return _coefficients.Select(x => x.ToBytes()).Flatten().ToArray();
-        }
+        //public byte[] ToBytes()
+        //{
+        //    return _coefficients.Select(x => x.ToBytes()).Flatten().ToArray();
+        //}
 
         public static Commitment FromBytes(IEnumerable<byte> buffer)
         {
@@ -80,14 +85,14 @@ namespace Lachain.Consensus.ThresholdKeygen.Data
 
         public bool Equals(Commitment? other)
         {
-            if (ReferenceEquals(null, other)) return false;
+            if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
             return _coefficients.SequenceEqual(other._coefficients);
         }
 
         public override bool Equals(object? obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
             return Equals((Commitment) obj);
@@ -96,6 +101,26 @@ namespace Lachain.Consensus.ThresholdKeygen.Data
         public override int GetHashCode()
         {
             return _coefficients.GetHashCode();
+        }
+
+        public byte[] ToBytes()
+        {
+            using var ms = new MemoryStream();
+            var serializer = new DataContractSerializer(typeof(Commitment));
+            serializer.WriteObject(ms, this);
+            return ms.ToArray();
+        }
+
+        public static Commitment? FromBytes(ReadOnlyMemory<byte> bytes)
+        {
+            if(bytes.ToArray() == null)
+            {
+                return default;
+            }
+            using var memStream = new MemoryStream(bytes.ToArray());
+            var serializer = new DataContractSerializer(typeof(Commitment));
+            var obj = (Commitment?)serializer.ReadObject(memStream);
+            return obj;
         }
     }
 }
