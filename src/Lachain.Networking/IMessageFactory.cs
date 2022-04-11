917323933cd59f313ec4d8eed1406175f8daf149
tbssajal <tariq.salam@latoken.com>
2022-04-11 16:49:09 +0600

    added comment documentation

--- src/Lachain.Networking/IMessageFactory.cs
+++ src/Lachain.Networking/IMessageFactory.cs
@@ -15,8 +15,30 @@ namespace Lachain.Networking
         NetworkMessage SyncPoolRequest(IEnumerable<UInt256> hashes);
         NetworkMessage SyncPoolReply(IEnumerable<TransactionReceipt> transactions);
         NetworkMessage SyncBlocksRequest(ulong fromHeight, ulong toHeight);
+        /// <summary>
+        /// Formats a NetworkMessage to request the root hash of a snapshot
+        /// </summary>
+        /// <param name = "block"> Block id </param>
+        /// <param name = "trieName"> Trie Name </param>
+        /// <returns>
+        /// NetworkMessage
+        /// </returns>
         NetworkMessage RootHashByTrieNameRequest(ulong block, string trieName);
+        /// <summary>
+        /// Formats a NetworkMessage to request blocks in batch
+        /// </summary>
+        /// <param name = "blockNumbers"> Block id </param>
+        /// <returns>
+        /// NetworkMessage
+        /// </returns>
         NetworkMessage BlockBatchRequest(List<ulong> blockNumbers);
+        /// <summary>
+        /// Formats a NetworkMessage to request trie-nodes in batch
+        /// </summary>
+        /// <param name = "nodeHashes"> Node hash </param>
+        /// <returns>
+        /// NetworkMessage
+        /// </returns>
         NetworkMessage TrieNodeByHashRequest(List<UInt256> nodeHashes);
 
         MessageBatch MessagesBatch(IEnumerable<NetworkMessage> messages);
