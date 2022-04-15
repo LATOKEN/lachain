7e4f5fac49671a2b3ee0c623412b38718367311a
tbssajal <tariq.salam@latoken.com>
2022-04-15 19:45:50 +0600

    completed checkpoint related methods

--- src/Lachain.Storage/Repositories/BlockCheckpointRepository.cs
+++ src/Lachain.Storage/Repositories/BlockCheckpointRepository.cs
@@ -1,14 +1,18 @@
-using System.Linq;
+using System;
 using Lachain.Proto;
 using Lachain.Utility.Serialization;
 using Lachain.Utility.Utils;
+using Lachain.Storage.State;
+using Lachain.Logger;
 
 namespace Lachain.Storage.Repositories
 {
     public class BlockCheckpointRepository : IBlockCheckpointRepository
     {
+        private static readonly ILogger<BlockCheckpointRepository> Logger = LoggerFactory.GetLoggerForClass<BlockCheckpointRepository>();
         private readonly IRocksDbContext _rocksDbContext;
         private readonly ISnapshotIndexRepository _snapshotIndexer;
+
         public BlockCheckpointRepository(
             IRocksDbContext rocksDbContext,
             ISnapshotIndexRepository snapshotIndexer
@@ -17,14 +21,52 @@ namespace Lachain.Storage.Repositories
             _rocksDbContext = rocksDbContext;
             _snapshotIndexer = snapshotIndexer;
         }
-        public ulong FetchCheckpointBlockId()
+
+        public ulong? FetchCheckpointBlockId()
         {
-            return 0;
-            // TODO: get block id
+            var key = EntryPrefix.CheckpointBlockHeight.BuildPrefix();
+            var blockId = _rocksDbContext.Get(key);
+            if (blockId is null) return null;
+            return UInt64Utils.FromBytes(blockId);
         }
+
+        public UInt256? FetchCheckpointBlockHash()
+        {
+            var key = EntryPrefix.CheckpointBlockHash.BuildPrefix();
+            var blockHash = _rocksDbContext.Get(key);
+            if (blockHash is null) return null;
+            return blockHash.ToUInt256();
+        }
+        
+        public UInt256? FetchSnapshotStateHash(RepositoryType repositoryId)
+        {
+            var key = EntryPrefix.CheckpointSnapshotState.BuildPrefix((uint) repositoryId);
+            var stateHash = _rocksDbContext.Get(key);
+            if (stateHash is null) return null;
+            return stateHash.ToUInt256();
+        }
+
         public void SaveCheckpoint(Block block)
         {
-            RocksDbAtomicWrite batch = new RocksDbAtomicWrite(_rocksDbContext);
+            try
+            {
+                var batch = new RocksDbAtomicWrite(_rocksDbContext);
+                var blockchainSnapshot = _snapshotIndexer.GetSnapshotForBlock(block.Header.Index);
+                var snapshots = blockchainSnapshot.GetAllSnapshot();
+                foreach (var snapshot in snapshots)
+                {
+                    if (snapshot.RepositoryId == (uint) RepositoryType.BlockRepository) continue;
+                    SaveSnapshotStateHash(batch, snapshot);
+                }
+                SaveBlockId(batch, block.Header.Index);
+                SaveBlockHash(batch, block.Hash);
+                batch.Commit();
+            }
+            catch (Exception exception)
+            {
+                Logger.LogWarning($"Could not save checkpoint for block {block.Header.Index}"
+                    +$" with hash {block.Hash.ToHex()}. Exception: {exception}");
+            }
         }
 
         private void SaveBlockId(RocksDbAtomicWrite batch, ulong blockId)
@@ -41,9 +83,11 @@ namespace Lachain.Storage.Repositories
             batch.Put(key, value);
         }
 
-        private void SaveBalanceState(RocksDbAtomicWrite batch, UInt256 stateHash)
+        private void SaveSnapshotStateHash(RocksDbAtomicWrite batch, ISnapshot snapshot)
         {
-            
+            var key = EntryPrefix.CheckpointSnapshotState.BuildPrefix(snapshot.RepositoryId);
+            var stateHash = snapshot.Hash.ToBytes();
+            batch.Put(key, stateHash);
         }
     }
 }
