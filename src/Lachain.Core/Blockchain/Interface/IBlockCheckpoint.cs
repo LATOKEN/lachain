95be4afecd0e5dba261d786d8a5433355ecb9417
tbssajal <tariq.salam@latoken.com>
2022-04-15 19:45:50 +0600

    completed checkpoint related methods

--- src/Lachain.Core/Blockchain/Interface/IBlockCheckpoint.cs
+++ src/Lachain.Core/Blockchain/Interface/IBlockCheckpoint.cs
@@ -1,13 +1,30 @@
 using Lachain.Proto;
+using Lachain.Storage.State;
 
 namespace Lachain.Core.Blockchain.Operations
 {
     public interface IBlockCheckpoint
     {
         /// <returns>
-        /// Block index of the last checkpoint
+        /// Block index of the last checkpoint, null if no checkpoint was saved
         /// </returns>
-        ulong CheckpointBlockId { get; }
+        ulong? CheckpointBlockId { get; }
+        /// <returns>
+        /// Block hash of the last checkpoint, null if no checkpoint was saved
+        /// </returns>
+        UInt256? CheckpointBlockHash { get; }
+        /// <summary>
+        /// Fetches the last checkpoint-block, null if no checkpoint was saved
+        /// </summary>
+        Block? GetCheckPointBlock();
+        /// <summary>
+        /// Takes the snapshot repository type as input and returns the state hash of that snapshot of the last checkpoint
+        /// </summary>
+        /// <param name = "snapshotType"> Repository type of one of these snapshots: balance, contract, event, storage, transaction, validator </param>
+        /// <returns>
+        /// State hash of last checkpoint
+        /// </returns>
+        UInt256? GetStateHashForSnapshot(RepositoryType snapshotType);
         /// <returns>
         /// The period to updated the checkpoint
         /// </returns>
@@ -15,10 +32,13 @@ namespace Lachain.Core.Blockchain.Operations
         /// <summary>
         /// Takes the given block as a checkpoint-block and writes necessary information in DB
         /// </summary>
-        void SaveCheckpointBlock(Block block);
+        void SaveCheckpoint(Block block);
         /// <summary>
-        /// Fetches the last checkpoint-block
+        /// Checks all checkpoint information: block index, hash and state hash for all six snapshots
         /// </summary>
-        Block? FetchCheckPointBlock();
+        /// <returns>
+        /// True if consistent, False otherwise
+        /// </returns>
+        bool IsCheckpointConsistent();
     }
 }
