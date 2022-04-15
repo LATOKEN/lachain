6d87ba7ef6f25f2b97380239bb862d1fa71b51fc
tbssajal <tariq.salam@latoken.com>
2022-04-15 19:45:50 +0600

    completed checkpoint related methods

--- src/Lachain.Storage/Repositories/IBlockCheckpointRepository.cs
+++ src/Lachain.Storage/Repositories/IBlockCheckpointRepository.cs
@@ -1,3 +1,4 @@
+using Lachain.Storage.State;
 using Lachain.Proto;
 
 namespace Lachain.Storage.Repositories
@@ -5,12 +6,24 @@ namespace Lachain.Storage.Repositories
     public interface IBlockCheckpointRepository
     {
         /// <returns>
-        /// Block Id of last checkpoint
-        /// </returns>
-        ulong FetchCheckpointBlockId();
-        /// <returns>
         /// Saves Checkpoint
         /// </returns>
         void SaveCheckpoint(Block block);
+        /// <returns>
+        /// Block Id of last checkpoint, null if no checkpoint was saved
+        /// </returns>
+        ulong? FetchCheckpointBlockId();
+        /// <returns>
+        /// Block hash of last checkpoint, null if no checkpoint was saved
+        /// </returns>   
+        UInt256? FetchCheckpointBlockHash();
+        /// <summary>
+        /// Takes the snapshot repository type as input and fetches the state hash of that snapshot of the last checkpoint
+        /// </summary>
+        /// <param name = "repositoryId"> Repository type of one of these snapshots: balance, contract, event, storage, transaction, validator </param>
+        /// <returns>
+        /// State hash of last checkpoint
+        /// </returns>
+        UInt256? FetchSnapshotStateHash(RepositoryType repositoryId);
     }
 }
