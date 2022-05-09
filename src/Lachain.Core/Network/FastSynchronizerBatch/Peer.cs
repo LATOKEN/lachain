613c5ff4f2579c14f46d041a9070d1614ce71c77
tbssajal <tariq.salam@latoken.com>
2022-05-09 22:23:00 +0600

    Changing downloading methods of fast sync

--- src/Lachain.Core/Network/FastSynchronizerBatch/Peer.cs
+++ src/Lachain.Core/Network/FastSynchronizerBatch/Peer.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lachain.Proto;
 
 namespace Lachain.Core.Network.FastSynchronizerBatch
 {
-    class Peer
+    class Peer : IEquatable<Peer>
     {
-        public string _url; 
-        public Peer(string url)
+        public ECDSAPublicKey _publicKey;
+        public string _url;
+        public Peer(ECDSAPublicKey publicKey)
         {
-            _url = url;
+            _publicKey = publicKey;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return _publicKey.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _publicKey.GetHashCode();
+        }
+
+        public bool Equals(Peer? peer)
+        {
+            return !(peer is null) && _publicKey.Equals(peer._publicKey);
         }
     }
 }
