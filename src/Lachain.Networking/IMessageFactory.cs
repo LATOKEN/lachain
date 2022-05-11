99b06d090defdbdafab93ff4a5c3a01dc2e33563
tbssajal <tariq.salam@latoken.com>
2022-05-11 22:41:38 +0600

    added request id in network message, fixed test

--- src/Lachain.Networking/IMessageFactory.cs
+++ src/Lachain.Networking/IMessageFactory.cs
@@ -28,18 +28,20 @@ namespace Lachain.Networking
         /// Formats a NetworkMessage to request blocks in batch
         /// </summary>
         /// <param name = "blockNumbers"> Block id </param>
+        /// <param name = "requestId"> Request Id </param>
         /// <returns>
         /// NetworkMessage
         /// </returns>
-        NetworkMessage BlockBatchRequest(List<ulong> blockNumbers);
+        NetworkMessage BlockBatchRequest(List<ulong> blockNumbers, ulong requestId);
         /// <summary>
         /// Formats a NetworkMessage to request trie-nodes in batch
         /// </summary>
         /// <param name = "nodeHashes"> Node hash </param>
+        /// <param name = "requestId"> Request Id </param>
         /// <returns>
         /// NetworkMessage
         /// </returns>
-        NetworkMessage TrieNodeByHashRequest(List<UInt256> nodeHashes);
+        NetworkMessage TrieNodeByHashRequest(List<UInt256> nodeHashes, ulong requestId);
         /// <summary>
         /// Formats a NetworkMessage to request checkpoint info
         /// </summary>
