8072388054fedd3cfef0891a8ed2728d8509b776
tbssajal <tariq.salam@latoken.com>
2022-05-12 23:05:22 +0600

    added interfaces for fastsync

---
+++ src/Lachain.Core/Network/FastSynchronizerBatch/INodeStorage.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Lachain.Proto;
+using Lachain.Storage.Repositories;
+using Lachain.Storage.State;
+using Lachain.Storage.Trie;
+
+namespace Lachain.Core.Network.FastSynchronizerBatch
+{
+    public interface INodeStorage
+    {
+        void Initialize(ulong blockNumber, UInt256 blockHash, List<(UInt256, CheckpointType)> stateHashes);
+        bool TryAddNode(TrieNodeInfo nodeInfo);
+        bool ExistNode(UInt256 nodeHash);
+        bool GetIdByHash(UInt256 nodeHash, out ulong id);
+        bool TryGetNode(ulong id, out IHashTrieNode? trieNode);
+        bool IsConsistent(TrieNodeInfo? node, out UInt256? nodeHash);
+        ulong GetBlockNumber();
+        UInt256? GetBlockHash();
+        UInt256? GetStateHash(CheckpointType checkpointType);
+        void AddBlock(IBlockSnapshot blockSnapshot, Block block);
+        void CommitIds();
+        void Commit();
+        int GetLastDownloadedTries();
+        void SetLastDownloadedTries(int downloaded);
+    }
+}
