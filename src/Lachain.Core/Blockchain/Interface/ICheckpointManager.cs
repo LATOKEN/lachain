6770d5b172751c5701c578c77de3e8bfb14530a0
tbssajal <tariq.salam@latoken.com>
2022-05-12 23:02:12 +0600

    Added helper method for checkpoint

--- src/Lachain.Core/Blockchain/Interface/ICheckpointManager.cs
+++ src/Lachain.Core/Blockchain/Interface/ICheckpointManager.cs
@@ -26,7 +26,23 @@ namespace Lachain.Core.Blockchain.Operations
         /// <returns>
         /// State hash of last checkpoint
         /// </returns>
-        UInt256? GetStateHashForSnapshot(RepositoryType snapshotType);
+        UInt256? GetStateHashForSnapshotType(RepositoryType snapshotType);
+        /// <summary>
+        /// Takes the checkpoint type of some snapshot as input and returns the state hash of that snapshot of the last checkpoint
+        /// </summary>
+        /// <param name = "checkpointType"> Checkpoint type for one of these snapshots: balance, contract, event, storage, transaction, validator </param>
+        /// <returns>
+        /// State hash of last checkpoint
+        /// </returns>
+        UInt256? GetStateHashForCheckpointType(CheckpointType checkpointType);
+        /// <summary>
+        /// Takes the checkpoint type of some snapshot as input and returns the state hash of that snapshot of the last checkpoint
+        /// </summary>
+        /// <param name = "snapshotName"> Trie name of one of these snapshots: balance, contract, event, storage, transaction, validator </param>
+        /// <returns>
+        /// State hash of last checkpoint
+        /// </returns>
+        UInt256? GetStateHashForSnapshotName(string snapshotName);
         /// <returns>
         /// The period to updated the checkpoint
         /// </returns>
@@ -34,6 +50,7 @@ namespace Lachain.Core.Blockchain.Operations
         /// <summary>
         /// Takes the given block as a checkpoint-block and writes necessary information in DB
         /// </summary>
+        /// <param name = "block"> Block </param>
         void SaveCheckpoint(Block block);
         /// <summary>
         /// Checks all checkpoint information: block index, hash and state hash for all six snapshots
@@ -42,6 +59,13 @@ namespace Lachain.Core.Blockchain.Operations
         /// True if consistent, False otherwise
         /// </returns>
         bool IsCheckpointConsistent();
-        List<CheckpointType> GetAllCheckpointTypes();
+        /// <summary>
+        /// Given some CheckpointType, gets the corresponding CheckpointInfo
+        /// </summary>
+        /// <param name = "checkpointType"> CheckpointType </param>
+        /// <returns>
+        /// True if consistent, False otherwise
+        /// </returns>
+        CheckpointInfo GetCheckpointInfo(CheckpointType checkpointType);
     }
 }
