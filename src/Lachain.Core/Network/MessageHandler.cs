a1195c362082ea50c58c9430465331a582189a59
tbssajal <tariq.salam@latoken.com>
2022-05-12 23:03:57 +0600

    removed unnecessary part of codes

--- src/Lachain.Core/Network/MessageHandler.cs
+++ src/Lachain.Core/Network/MessageHandler.cs
@@ -90,8 +90,6 @@ namespace Lachain.Core.Network
             _networkManager.OnSyncPoolRequest += OnSyncPoolRequest;
             _networkManager.OnSyncPoolReply += OnSyncPoolReply;
             _networkManager.OnConsensusMessage += OnConsensusMessage;
-            _networkManager.OnRootHashByTrieNameRequest += OnRootHashByTrieNameRequest;
-            _networkManager.OnRootHashByTrieNameReply += OnRootHashByTrieNameReply;
             _networkManager.OnBlockBatchRequest += OnBlockBatchRequest;
             _networkManager.OnBlockBatchReply += OnBlockBatchReply;
             _networkManager.OnTrieNodeByHashRequest += OnTrieNodeByHashRequest;
