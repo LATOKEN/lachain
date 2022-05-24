5cff3e5a2f118fdc9dc33b4c9b01062c3f7f0809
sgladkov <sergey.gladkov@latoken.com>
2022-05-24 17:27:54 +0300

    Set hardfork_9 height for mainnet

--- src/Lachain.Core/Config/ConfigManager.cs
+++ src/Lachain.Core/Config/ConfigManager.cs
@@ -318,7 +318,7 @@ namespace Lachain.Core.Config
                             throw new ApplicationException("No hardfork section in config");
             hardforks.Hardfork_9 ??= network.NetworkName switch
             {
-                "mainnet" => 3569350,
+                "mainnet" => 3693350,
                 "testnet" => 3095300,
                 "devnet" => 743530,
                 _ => 0
