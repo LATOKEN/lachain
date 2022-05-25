508b255dc2062df811d735fe8704650e859fb06c
sgladkov <sergey.gladkov@latoken.com>
2022-05-25 15:35:01 +0300

    use other config for test

--- test/Lachain.CoreTest/IntegrationTests/ValidatorStatusTests.cs
+++ test/Lachain.CoreTest/IntegrationTests/ValidatorStatusTests.cs
@@ -45,7 +45,7 @@ namespace Lachain.CoreTest.IntegrationTests
         public ValidatorStatusTest()
         {
             var containerBuilder = new SimpleInjectorContainerBuilder(new ConfigManager(
-                Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json"),
+                Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config2.json"),
                 new RunOptions()
             ));
 
