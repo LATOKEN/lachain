14defb93788a2e4cb60c4ee93d7a6750d1b93679
tbssajal <sajalbuet15@gmail.com>
2022-07-25 20:37:29 +0600

    added extra new lines

--- src/Lachain.Storage/State/TransactionSnapshot.cs
+++ src/Lachain.Storage/State/TransactionSnapshot.cs
@@ -20,6 +20,7 @@ namespace Lachain.Storage.State
         private readonly IStorageState _state;
 
         public ulong Version => _state.CurrentVersion;
+
         public uint RepositoryId => _state.RepositoryId;
 
         public TransactionSnapshot(IStorageState state)
