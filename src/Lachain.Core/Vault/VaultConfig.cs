4499762617fdbace3e1b641a35383642c7d3ada3
Pritom Kundu <anachored@gmail.com>
2022-10-20 12:09:47 +0600

    Add vault token and pass to config

--- src/Lachain.Core/Vault/VaultConfig.cs
+++ src/Lachain.Core/Vault/VaultConfig.cs
@@ -4,7 +4,9 @@ namespace Lachain.Core.Vault
 {
     public class VaultConfig
     {
-        [JsonProperty("usevault")] public bool? UseVault { get; set; }
+        [JsonProperty("useVault")] public bool? UseVault { get; set; }
+        [JsonProperty("vaultPath")] public string? VaultPath { get; set; }
+        [JsonProperty("vaultToken")] public string? VaultToken { get; set; }
         [JsonProperty("path")] public string? Path { get; set; }
         [JsonProperty("password")] public string? Password { get; set; }
     }
