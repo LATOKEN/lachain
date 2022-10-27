f472b40898340105fe3235de9f94180ea5f56cc3
tbssajal <sajalbuet15@gmail.com>
2022-10-27 21:51:47 +0600

    added enum and methods to handle different hardfork states

--- src/Lachain.Utility/ConsensusState.cs
+++ src/Lachain.Utility/ConsensusState.cs
@@ -8,6 +8,7 @@ namespace Lachain.Utility
 {
     public class ConsensusState
     {
+        // if number of validators is greater than 255, we need to change ConsensusState with a hardfork
         public ConsensusState(byte[] tpkePublicKey, byte[][] tpkeVerificationKeys, ValidatorCredentials[] validators)
         {
             TpkePublicKey = tpkePublicKey;
@@ -20,35 +21,49 @@ namespace Lachain.Utility
         public byte[][] TpkeVerificationKeys { get; }
         public ValidatorCredentials[] Validators { get; }
 
-        public byte[] ToBytes(bool useNewFormat)
+        private byte[] OldStateToBytes()
         {
-            var a = new List<byte[]> {TpkePublicKey};
-            if (useNewFormat)
-            {
-                a.Add(new byte[] {(byte) TpkeVerificationKeys.Length});
-                a.AddRange(TpkeVerificationKeys);
-            }
-            a.AddRange(Validators.Select(c => c.ToBytes()));
-            return RLP.EncodeList(a.Select(RLP.EncodeElement).ToArray());
+            var rawInfo = new List<byte[]> {TpkePublicKey};
+            rawInfo.AddRange(Validators.Select(c => c.ToBytes()));
+            return RLP.EncodeList(rawInfo.Select(RLP.EncodeElement).ToArray());
         }
 
-        public static ConsensusState FromBytes(ReadOnlySpan<byte> bytes)
+        private byte[] StateWithTpkeVerificationToBytes()
         {
-            var decoded = (RLPCollection) RLP.Decode(bytes.ToArray());
-            var tpkePubKey = decoded[0].RLPData;
-            if (decoded[1].RLPData.Length == 1) // new data format with verification keys
+            var rawInfo = new List<byte[]> {TpkePublicKey};
+            rawInfo.Add(new byte[] {(byte) TpkeVerificationKeys.Length});
+            rawInfo.AddRange(TpkeVerificationKeys);
+            rawInfo.AddRange(Validators.Select(c => c.ToBytes()));
+            return RLP.EncodeList(rawInfo.Select(RLP.EncodeElement).ToArray());
+        }
+
+        private byte[] StateWithOnlyValidatorInfoToBytes()
+        {
+            var rawInfo = new List<byte[]>();
+            rawInfo.Add(new byte[] {(byte) Validators.Length});
+            rawInfo.AddRange(Validators.Select(c => c.ToBytes()));
+            return RLP.EncodeList(rawInfo.Select(RLP.EncodeElement).ToArray());
+        }
+
+        public byte[] ToBytes(ConsensusStateStatus status)
+        {
+            switch (status)
             {
-                var keysNumber = decoded[1].RLPData[0];
-                var tpkeVerificationKeys = decoded.Skip(2).Take(keysNumber)
-                    .Select(x => x.RLPData)
-                    .ToArray();
-                var credentials = decoded.Skip(2 + keysNumber)
-                    .Select(x => x.RLPData)
-                    .Select(x => ValidatorCredentials.FromBytes(x))
-                    .ToArray();
-                return new ConsensusState(tpkePubKey, tpkeVerificationKeys, credentials);
+                case ConsensusStateStatus.OldState:
+                    return OldStateToBytes();
+                case ConsensusStateStatus.StateWithTpkeVerification:
+                    return StateWithTpkeVerificationToBytes();
+                case ConsensusStateStatus.StateWithOnlyValidatorInfo:
+                    return StateWithOnlyValidatorInfoToBytes();
+                default:
+                    throw new Exception($"Unhandled ConsensusStateStatus: {status}");
             }
-            // old data format without verification keys
+        }
+
+        // old state where we have tpke public key and validator credentials
+        public static ConsensusState LoadOldState(RLPCollection decoded)
+        {
+            var tpkePubKey = decoded[0].RLPData;
             var old_credentials = decoded.Skip(1)
                 .Select(x => x.RLPData)
                 .Select(x => ValidatorCredentials.FromBytes(x))
@@ -57,5 +72,53 @@ namespace Lachain.Utility
                 .Select(i => tpkePubKey).ToArray();
             return new ConsensusState(tpkePubKey, fakeTpkeVerificationKeys, old_credentials);
         }
+
+        // state where we have tpke public and verification keys and validator credentials
+        public static ConsensusState LoadStateWithTpkeVerification(RLPCollection decoded)
+        {
+            var tpkePubKey = decoded[0].RLPData;
+            var keysNumber = decoded[1].RLPData[0];
+            var tpkeVerificationKeys = decoded.Skip(2).Take(keysNumber)
+                .Select(x => x.RLPData)
+                .ToArray();
+            var credentials = decoded.Skip(2 + keysNumber)
+                .Select(x => x.RLPData)
+                .Select(x => ValidatorCredentials.FromBytes(x))
+                .ToArray();
+            return new ConsensusState(tpkePubKey, tpkeVerificationKeys, credentials);
+        }
+
+        // updated state where we have only validator credentials
+        public static ConsensusState LoadStateWithOnlyValidatorInfo(RLPCollection decoded)
+        {
+            var keysNumber = decoded[0].RLPData[0];
+            var credentials = decoded.Skip(1).Take(keysNumber)
+                .Select(x => x.RLPData)
+                .Select(x => ValidatorCredentials.FromBytes(x))
+                .ToArray();
+            return new ConsensusState(Array.Empty<byte>(), Array.Empty<byte[]>(), credentials);
+        }
+
+        public static ConsensusState FromBytes(ReadOnlySpan<byte> bytes)
+        {
+            var decoded = (RLPCollection) RLP.Decode(bytes.ToArray());
+            if (decoded[0].RLPData.Length == 1)
+            {
+                // updated data format with only validator info
+                return LoadStateWithOnlyValidatorInfo(decoded);
+            }
+            else if (decoded[1].RLPData.Length == 1) // new data format with verification keys
+            {
+                return LoadStateWithTpkeVerification(decoded);
+            }
+            else return LoadOldState(decoded);
+        }
+    }
+
+    public enum ConsensusStateStatus : byte
+    {
+        OldState = 0,
+        StateWithTpkeVerification = 1,
+        StateWithOnlyValidatorInfo = 2,
     }
 }
