1903889dd42151649f997de316dcec8f9caef43c
Pritom Kundu <anachored@gmail.com>
2022-11-03 19:40:19 +0600

    Add repository for message envelope

---
+++ src/Lachain.Storage/Repositories/IMessageEnvelopeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Lachain.Storage.Repositories
+{
+    public interface IMessageEnvelopeRepository
+    {
+        void SaveMessages(byte[] keygenState);
+        byte[] LoadMessages();        
+    }
+}
