bfd66b7d68dc1508d4453ce4b968f906a7aefb8e
tbssajal <sajalbuet15@gmail.com>
2022-11-15 00:22:14 +0600

    added request manager to direct requests to appropriate protocol handler

---
+++ src/Lachain.Consensus/RequestProtocols/RequestManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Lachain.Consensus.RequestProtocols.Protocols;
+using Lachain.Logger;
+using Lachain.Proto;
+
+namespace Lachain.Consensus.RequestProtocols
+{
+    public class RequestManager : IRequestManager
+    {
+        private static readonly ILogger<RequestManager> Logger = LoggerFactory.GetLoggerForClass<RequestManager>();
+        private int _validators = -1;
+        private readonly long _era;
+        private readonly IConsensusBroadcaster _broadcaster;
+        private readonly IDictionary<IProtocolIdentifier, IProtocolRequestHandler> _protocolHandler;
+        public RequestManager(IConsensusBroadcaster broadcaster, long era)
+        {
+            _broadcaster = broadcaster;
+            _era = era;
+            _protocolHandler = new ConcurrentDictionary<IProtocolIdentifier, IProtocolRequestHandler>();
+        }
+
+        public void Terminate()
+        {
+            foreach (var (_, handler) in _protocolHandler)
+            {
+                handler.Terminate();
+            }
+            _protocolHandler.Clear();
+            Logger.LogTrace($"Request manager for era {_era} terminated");
+        }
+
+        public void SetValidators(int validatorsCount)
+        {
+            _validators = validatorsCount;
+        }
+
+        public void RegisterProtocol(IProtocolIdentifier protocolId)
+        {
+            if (_validators == -1)
+                throw new Exception($"RequestManager not ready yet, validators count {_validators}");
+            if (_protocolHandler.TryGetValue(protocolId, out var _))
+            {
+                throw new Exception($"Protocol handler for protocolId {protocolId} already registered");
+            }
+
+            _protocolHandler[protocolId] = new ProtocolRequestHandler(protocolId, _validators);
+        }
+
+        public void MessageReceived(IProtocolIdentifier protocolId, int from, ConsensusMessage msg)
+        {
+            if (_protocolHandler.TryGetValue(protocolId, out var handler))
+            {
+                handler.MessageReceived(from, msg);
+            }
+            else
+                throw new Exception($"Protocol handler for protocolId {protocolId} not registered but External message is received");
+        }
+    }
+}
