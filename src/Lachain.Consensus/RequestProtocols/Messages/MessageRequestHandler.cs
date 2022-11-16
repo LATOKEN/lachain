0f3d7c36941436a9d457cbdb1307b2a11ae7b1d2
tbssajal <sajalbuet15@gmail.com>
2022-11-16 17:20:57 +0600

    made abstract class and added sync attr

--- src/Lachain.Consensus/RequestProtocols/Messages/MessageRequestHandler.cs
+++ src/Lachain.Consensus/RequestProtocols/Messages/MessageRequestHandler.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Collections.Generic;
-using Lachain.Consensus.BinaryAgreement;
-using Lachain.Consensus.CommonCoin;
-using Lachain.Consensus.HoneyBadger;
-using Lachain.Consensus.ReliableBroadcast;
-using Lachain.Consensus.RootProtocol;
+using System.Runtime.CompilerServices;
 using Lachain.Logger;
 using Lachain.Proto;
 
 namespace Lachain.Consensus.RequestProtocols.Messages
 {
-    public class MessageRequestHandler : IMessageRequestHandler
+    public abstract class MessageRequestHandler : IMessageRequestHandler
     {
         private static readonly ILogger<MessageRequestHandler> Logger = LoggerFactory.GetLoggerForClass<MessageRequestHandler>();
         private MessageStatus[][] _status;
@@ -42,6 +38,7 @@ namespace Lachain.Consensus.RequestProtocols.Messages
             _remainingMsges = _msgCount;
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public void Terminate()
         {
             if (_terminated)
@@ -51,96 +48,21 @@ namespace Lachain.Consensus.RequestProtocols.Messages
             _status = new MessageStatus[0][];
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public void MessageReceived(int from, ConsensusMessage msg, RequestType type)
         {
             if (_terminated)
                 return;
             if (type != _type)
                 throw new Exception($"message type {type} routed to message handler {_type}");
-            switch (type)
-            {
-                case RequestType.Aux:
-                    HandleAuxMessage(from, msg.Aux);
-                    break;
-                case RequestType.Bval:
-                    HandleBValMessage(from, msg.Bval);
-                    break;
-                case RequestType.Coin:
-                    HandleCoinMessage(from, msg.Coin);
-                    break;
-                case RequestType.Conf:
-                    HandleConfMessage(from, msg.Conf);
-                    break;
-                case RequestType.Decrypted:
-                    HandleDecryptedMessage(from, msg.Decrypted);
-                    break;
-                case RequestType.Echo:
-                    HandleEchoMessage(from, msg.EchoMessage);
-                    break;
-                case RequestType.Ready:
-                    HandleReadyMessage(from, msg.ReadyMessage);
-                    break;
-                case RequestType.SignedHeader:
-                    HandleSignedHeaderMessage(from, msg.SignedHeaderMessage);
-                    break;
-                case RequestType.Val:
-                    HandleValMessage(from, msg.ValMessage);
-                    break;
-                default:
-                    throw new Exception($"Not implemented consensus message {msg.PayloadCase}");
-            }
+            HandleReceivedMessage(from, msg);
         }
 
-        private void MessageReceived(int validatorId, int msgId)
+        public void MessageReceived(int validatorId, int msgId)
         {
             if (_status[validatorId][msgId] != MessageStatus.Received)
                 _remainingMsges--;
             _status[validatorId][msgId] = MessageStatus.Received;
-        }
-
-        private void HandleAuxMessage(int from, AuxMessage _)
-        {
-            MessageReceived(from, 0);
-        }
-
-        private void HandleBValMessage(int from, BValMessage msg)
-        {
-            MessageReceived(from, msg.Value ? 1 : 0);
-        }
-
-        private void HandleConfMessage(int from, ConfMessage _)
-        {
-            MessageReceived(from, 0);
-        }
-
-        private void HandleCoinMessage(int from, CommonCoinMessage _)
-        {
-            MessageReceived(from, 0);
-        }
-
-        private void HandleEchoMessage(int from, ECHOMessage _)
-        {
-            MessageReceived(from, 0);
-        }
-
-        private void HandleReadyMessage(int from, ReadyMessage _)
-        {
-            MessageReceived(from, 0);
-        }
-
-        private void HandleValMessage(int _from, ValMessage _)
-        {
-            MessageReceived(0, 0);
-        }
-
-        private void HandleDecryptedMessage(int from, TPKEPartiallyDecryptedShareMessage msg)
-        {
-            MessageReceived(from, msg.ShareId);
-        }
-
-        private void HandleSignedHeaderMessage(int from, SignedHeaderMessage _)
-        {
-            MessageReceived(from, 0);
         }
 
         public bool IsProtocolComplete()
@@ -148,6 +70,7 @@ namespace Lachain.Consensus.RequestProtocols.Messages
             return _remainingMsges == _msgCount;
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public List<(ConsensusMessage, int)> GetRequests(IProtocolIdentifier protocolId, int requestCount)
         {
             var requests = new List<(ConsensusMessage, int)>();
@@ -185,186 +108,7 @@ namespace Lachain.Consensus.RequestProtocols.Messages
             return requests;
         }
 
-        private ConsensusMessage CreateConsensusMessage(IProtocolIdentifier protocolId, int msgId)
-        {
-            var wrongProtocolWarning = $"wrong protcolId {protocolId} for request type {_type}";
-            switch (_type)
-            {
-                case RequestType.Aux:
-                    return CreateAuxRequest(protocolId as BinaryBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Bval:
-                    return CreateBValRequest(protocolId as BinaryBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Conf:
-                    return CreateConfRequest(protocolId as BinaryBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Coin:
-                    return CreateCoinRequest(protocolId as CoinId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Decrypted:
-                    return CreateDecryptedRequest(protocolId as HoneyBadgerId ?? throw new Exception(wrongProtocolWarning), msgId);
-                case RequestType.Echo:
-                    return CreateEchoRequest(protocolId as ReliableBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Ready:
-                    return CreateReadyRequest(protocolId as ReliableBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.Val:
-                    return CreateValRequest(protocolId as ReliableBroadcastId ?? throw new Exception(wrongProtocolWarning));
-                case RequestType.SignedHeader:
-                    return CreateSignedHeaderRequest(protocolId as RootProtocolId ?? throw new Exception(wrongProtocolWarning));
-                default:
-                    throw new Exception($"Not implemented request type {_type}");
-            }
-        }
-
-        private ConsensusMessage CreateAuxRequest(BinaryBroadcastId id)
-        {
-            if (_type != RequestType.Aux)
-                throw new Exception($"Aux request routed to {_type} message handler");
-            var auxRequest = new RequestAuxMessage
-            {
-                Agreement = (int) id.Agreement,
-                Epoch = (int) id.Epoch
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestAux = auxRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateBValRequest(BinaryBroadcastId id)
-        {
-            if (_type != RequestType.Bval)
-                throw new Exception($"BVal request routed to {_type} message handler");
-            var bvalRequest = new RequestBValMessage
-            {
-                Agreement = (int) id.Agreement,
-                Epoch = (int) id.Epoch
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestBval = bvalRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateConfRequest(BinaryBroadcastId id)
-        {
-            if (_type != RequestType.Conf)
-                throw new Exception($"Conf request routed to {_type} message handler");
-            var confRequest = new RequestConfMessage
-            {
-                Agreement = (int) id.Agreement,
-                Epoch = (int) id.Epoch
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestConf = confRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateCoinRequest(CoinId id)
-        {
-            if (_type != RequestType.Coin)
-                throw new Exception($"Coin request routed to {_type} message handler");
-            var coinRequest = new RequestCommonCoinMessage
-            {
-                Agreement = (int) id.Agreement,
-                Epoch = (int) id.Epoch
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestCoin = coinRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateValRequest(ReliableBroadcastId id)
-        {
-            if (_type != RequestType.Val)
-                throw new Exception($"Val request routed to {_type} message handler");
-            var valRequest = new RequestValMessage
-            {
-                SenderId = id.SenderId
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestVal = valRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateEchoRequest(ReliableBroadcastId id)
-        {
-            if (_type != RequestType.Echo)
-                throw new Exception($"Echo request routed to {_type} message handler");
-            var echoRequest = new RequestECHOMessage
-            {
-                SenderId = id.SenderId
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestEcho = echoRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateReadyRequest(ReliableBroadcastId id)
-        {
-            if (_type != RequestType.Ready)
-                throw new Exception($"Ready request routed to {_type} message handler");
-            var readyRequest = new RequestReadyMessage
-            {
-                SenderId = id.SenderId
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestReady = readyRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateDecryptedRequest(HoneyBadgerId _, int shareId)
-        {
-            if (_type != RequestType.Decrypted)
-                throw new Exception($"Decrypted request routed to {_type} message handler");
-            var decryptedRequest = new RequestTPKEPartiallyDecryptedShareMessage
-            {
-                ShareId = shareId
-            };
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestDecrypted = decryptedRequest
-                }
-            };
-        }
-
-        private ConsensusMessage CreateSignedHeaderRequest(RootProtocolId _)
-        {
-            if (_type != RequestType.SignedHeader)
-                throw new Exception($"Signed header request routed to {_type} message handler");
-            var headerRequest = new RequestSignedHeaderMessage();
-            return new ConsensusMessage
-            {
-                RequestConsensus = new RequestConsensusMessage
-                {
-                    RequestSignedHeader = headerRequest
-                }
-            };
-        }
+        public abstract ConsensusMessage CreateConsensusMessage(IProtocolIdentifier protocolId, int msgId);
+        public abstract void HandleReceivedMessage(int from, ConsensusMessage msg);
     }
 }
