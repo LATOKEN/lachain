283c91750c247025f134b5e150e0b3373a619c8c
tbssajal <sajalbuet15@gmail.com>
2022-11-16 20:26:30 +0600

    added method to create protocolId on request

--- src/Lachain.Consensus/RequestProtocols/Messages/Requests/DecryptedRequest.cs
+++ src/Lachain.Consensus/RequestProtocols/Messages/Requests/DecryptedRequest.cs
@@ -34,5 +34,12 @@ namespace Lachain.Consensus.RequestProtocols.Messages.Requests
                 }
             };
         }
+
+        public static HoneyBadgerId CreateProtocolId(RequestConsensusMessage msg, long era)
+        {
+            if (msg.PayloadCase != RequestConsensusMessage.PayloadOneofCase.RequestAux)
+                throw new Exception($"{msg.PayloadCase} routed to Aux Request");
+            return new HoneyBadgerId(era);
+        }
     }
 }
