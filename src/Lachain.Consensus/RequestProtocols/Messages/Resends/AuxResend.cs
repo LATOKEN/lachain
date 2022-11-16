dc95ac67940318d2b8fab187461941fa2b741e35
tbssajal <sajalbuet15@gmail.com>
2022-11-16 20:25:08 +0600

    added method to handle request

--- src/Lachain.Consensus/RequestProtocols/Messages/Resends/AuxResend.cs
+++ src/Lachain.Consensus/RequestProtocols/Messages/Resends/AuxResend.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Lachain.Proto;
 
 namespace Lachain.Consensus.RequestProtocols.Messages.Resends
@@ -16,6 +17,20 @@ namespace Lachain.Consensus.RequestProtocols.Messages.Resends
             if (msg.PayloadCase != ConsensusMessage.PayloadOneofCase.Aux)
                 throw new Exception($"{msg.PayloadCase} message routed to Aux Resend");
             SaveMessage(validator, 0, msg);
+        }
+
+        protected override List<ConsensusMessage?> HandleRequestMessage(int from, RequestConsensusMessage msg)
+        {
+            if (msg.PayloadCase != RequestConsensusMessage.PayloadOneofCase.RequestAux)
+                throw new Exception($"{msg.PayloadCase} routed to Aux Resend");
+            var msgs = new List<ConsensusMessage?>();
+            var msgIds = new List<int>();
+            msgIds.Add(0);
+            foreach (var id in msgIds)
+            {
+                msgs.Add(GetMessage(from, id));
+            }
+            return msgs;
         }
     }
 }
