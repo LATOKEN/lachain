8efed0bc1b3d0e5c2cb0cb51eb4994a34b5eeae8
Pritom Kundu <anachored@gmail.com>
2022-11-22 17:25:23 +0100

    Add request identifier class

---
+++ src/Lachain.Networking/RequestIdentifier.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+namespace Lachain.Networking
+{
+    public class RequestIdentifier
+    {
+        private ulong RequestId { get; }
+        private byte[] PeerPublicKey { get; }
+
+        public RequestIdentifier(ulong requestId, byte[] peerPublicKey)
+        {
+            RequestId = requestId;
+            PeerPublicKey = peerPublicKey;
+        }
+
+        protected bool Equals(RequestIdentifier other)
+        {
+            return RequestId == other.RequestId && PeerPublicKey.SequenceEqual(other.PeerPublicKey);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((RequestIdentifier)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RequestId, PeerPublicKey.Length);
+        }
+    }
+}
