95ed246e139bbaa186fe09fea5fbbf545a120b8b
tbssajal <sajalbuet15@gmail.com>
2022-12-08 20:51:31 +0600

    fixed tests

--- test/Lachain.CoreTest/Network/MessageHandlerTest.cs
+++ test/Lachain.CoreTest/Network/MessageHandlerTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Lachain.Core.CLI;
 using Lachain.Core.Config;
@@ -8,6 +10,8 @@ using Lachain.Core.DI.Modules;
 using Lachain.Core.DI.SimpleInjector;
 using Lachain.Core.Network;
 using Lachain.Proto;
+using Lachain.Storage.State;
+using Lachain.Utility.Utils;
 using Lachain.UtilityTest;
 using NUnit.Framework;
 
@@ -16,8 +20,9 @@ namespace Lachain.CoreTest.Network
     
     public class MessageHandlerTest
     {
-        private IContainer _container;
-        private MessageHandler _handler;
+        private IContainer _container = null!;
+        private MessageHandler _handler = null!;
+        private IStateManager _stateManager = null!;
 
         [SetUp]
         public void Setup()
@@ -34,6 +39,7 @@ namespace Lachain.CoreTest.Network
             containerBuilder.RegisterModule<NetworkModule>();
             _container = containerBuilder.Build();
             _handler = (MessageHandler) _container.Resolve<IMessageHandler>();
+            _stateManager = _container.Resolve<IStateManager>();
         }
         
         
@@ -47,29 +53,62 @@ namespace Lachain.CoreTest.Network
         [Test]
         public void SyncBlockRequestTest()
         {
-            var snap = new BlockSnapsotProxy();
-            for (int i = 0; i < 20; i++)  {
-                snap.AddBlock(new Block());
+            var snap = _stateManager.NewSnapshot().Blocks;
+            ulong myHeight = 20;
+            var blocks = new List<Block>();
+            for (ulong i = 0; i <= myHeight; i++)  {
+                var block = new Block
+                {
+                    Header = new BlockHeader
+                    {
+                        Index = i
+                    },
+                    Hash = TestUtils.GetRandomBytes(32).ToUInt256()
+                };
+                snap.AddBlock(block);
+                blocks.Add(block);
             }
+            _stateManager.Approve();
 
             var request = new SyncBlocksRequest();
-            request.FromHeight = 0;
+            request.FromHeight = 1;
             request.ToHeight = 10;
-            Assert.DoesNotThrow(() => _handler.ValidateSyncBlocksRequest(request, snap));
+            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, _stateManager.LastApprovedSnapshot.Blocks));
             
             request.FromHeight = 0;
             request.ToHeight = 30;
-            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, snap));
+            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, _stateManager.LastApprovedSnapshot.Blocks));
 
             
             request.FromHeight = 0;
             request.ToHeight = 1000000;
-            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, snap));
+            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, _stateManager.LastApprovedSnapshot.Blocks));
             
             request.FromHeight = 10;
             request.ToHeight = 0;
-            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, snap));
-            
+            Assert.Throws<ArgumentException>(() => _handler.ValidateSyncBlocksRequest(request, _stateManager.LastApprovedSnapshot.Blocks));
+
+            var newReq = new SyncBlocksRequest
+            {
+                FromHeight = 10,
+                ToHeight = 19,
+                Proof = 
+                {
+                    blocks.Take(10).Select(
+                        block => new BlockInfo
+                        {
+                            Block = block,
+                            Transactions = 
+                            {
+                                block.TransactionHashes.Select(
+                                    tx => _stateManager.LastApprovedSnapshot.Transactions.GetTransactionByHash(tx)
+                                )
+                            }
+                        }
+                    )
+                }
+            };
+            Assert.DoesNotThrow(() => _handler.ValidateSyncBlocksRequest(newReq, _stateManager.LastApprovedSnapshot.Blocks));
         }
         
         [Test]
